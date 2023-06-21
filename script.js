c18111cfe51edc25720da30d67d84cfa86b059ff
Rafael Wartha <113060308+rafaelwartha@users.noreply.github.com>
2023-06-21 10:42:48 +0100

    teste

--- script.js
+++ script.js
@@ -1,54 +1,56 @@
 // Aguarde o carregamento completo da página
 window.addEventListener('load', function() {
-    // Elemento alvo
-    var targetElement = document.querySelector('.name');
-    
-    // Texto a ser digitado letra por letra
-    var text = "I'm Rafael Wartha...";
-    
-    // Intervalo entre cada letra (em milissegundos)
-    var interval = 100;
-  
-    // Variável para controlar a posição atual do texto
-    var currentPos = 0;
-  
-    // Função para adicionar a próxima letra
-    function addLetter() {
-      if (currentPos < text.length) {
-        targetElement.textContent += text[currentPos];
-        currentPos++;
-        setTimeout(addLetter, interval);
-      }
-    }
-  
-    // Iniciar a digitação letra por letra
-    addLetter();
-  });
+  // Elemento alvo para o nome
+  var nameElement = document.querySelector('.name');
 
-  window.addEventListener('load', function() {
-    // Elemento alvo
-    var targetElement = document.querySelector('.p-main');
-    
+  // Texto do nome a ser digitado letra por letra
+  var nameText = "I'm Rafael Wartha...";
+
+  // Intervalo entre cada letra do nome (em milissegundos)
+  var nameInterval = 100;
+
+  // Variável para controlar a posição atual do nome
+  var namePos = 0;
+
+  // Função para adicionar a próxima letra do nome
+  function addNameLetter() {
+    if (namePos < nameText.length) {
+      nameElement.textContent += nameText[namePos];
+      namePos++;
+      setTimeout(addNameLetter, nameInterval);
+    } else {
+      // Após escrever o nome, inicia a escrita do texto
+      addTextLetter();
+    }
+  }
+
+  // Função para escrever o texto
+  function addTextLetter() {
+    // Elemento alvo para o texto
+    var textElement = document.querySelector('.p-main');
+
     // Texto a ser digitado letra por letra
     var text = "Currently, I am a student studying IT with a specific focus on back-end development using PHP. My main goal is to enhance my skills and knowledge in this field.";
-    
-    // Intervalo entre cada letra (em milissegundos)
+
+    // Intervalo entre cada letra do texto (em milissegundos)
     var interval = 20;
-  
+
     // Variável para controlar a posição atual do texto
     var currentPos = 0;
-  
-    // Função para adicionar a próxima letra
+
+    // Função para adicionar a próxima letra do texto
     function addLetter() {
       if (currentPos < text.length) {
-        targetElement.textContent += text[currentPos];
+        textElement.textContent += text[currentPos];
         currentPos++;
         setTimeout(addLetter, interval);
       }
     }
-  
-    // Iniciar a digitação letra por letra
+
+    // Iniciar a digitação letra por letra do texto
     addLetter();
-  });
-  
-  
+  }
+
+  // Iniciar a digitação letra por letra do nome
+  addNameLetter();
+});
